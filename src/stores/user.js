62765655611e0aac4906682ef1baeaf78da8131b
edankap <edankap20@gmail.com>
2023-04-26 12:17:00 +0200

    create SignOut

--- src/stores/user.js
+++ src/stores/user.js
@@ -10,10 +10,10 @@ export default defineStore('userStore', {
     actions: {
         // para saber si tenemos un usuario que haya iniciado sesión // 
         async fetchUser() {
-            const { data: { user } } = await supabase.auth.getUser()
-            this.user = user
+            const { data: { user } } = await supabase.auth.getUser();
+            this.user = user;
             if(!user) throw new Error('El usuario no existe');
-            console.log(user)
+            console.log(user);
         },
         //método de crear un nuevo usuario que lo tomo de la doc de SupaBase
         async registerUser( { email, password }) {
@@ -32,6 +32,11 @@ export default defineStore('userStore', {
               });
               if (error) throw error;
               if (user) this.user = user;
-        }
+        },
+        async signOut() {
+            const { error } = await supabase.auth.signOut();
+            if (error) throw error;
+            this.user = null;
+        },
     },
 })
