be5b086df388e6d3c94bfeed1c031d8b11de3174
edankap <edankap20@gmail.com>
2023-05-01 22:18:24 +0200

    done created tasks and some features in the tables

--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -15,9 +15,10 @@ export default {
     computed: {
         ...mapState(TodoStore, ['tasksList']),
         ...mapState(UserStore, ['user']),
+        ...mapState(TodoStore, ['completedTaskList']),
     },
     methods: {
-        ...mapActions(TodoStore, ['_fetchAllTasks', '_addNewTask', '_editTask']),
+        ...mapActions(TodoStore, ['_fetchAllTasks', '_addNewTask', '_editTask', '_completeTask', '_incompleteTask', '_eraseTask', '_fetchTasks']),
         async _handleNewTask() {
             try {
                 await this._addNewTask({ title: this.newTaskTitle, user_id: this.user.id })
@@ -39,50 +40,118 @@ export default {
             } catch (err) {
                 console.error(err)
             }
-        }
+        },
+        async _handleCompleteTask(todo) {
+            try {
+                await this._completeTask({ id: todo.id })
+                todo.is_complete = true
+                this.completedTaskList.push(todo);
+            } catch (err) {
+                console.error(err)
+            }
+            /*             this._fetchAllTasks() */
+        },
+        async _handleIncompleteTask(todo) {
+            try {
+                await this._incompleteTask({ id: todo.id })
+            } catch (err) {
+                console.error(err)
+            }
+        },
+        async _handleEraseTask(todo) {
+            try {
+                await this._eraseTask(todo.id)
+                await this._fetchTasks();
+            } catch (err) {
+                console.error(err)
+            }
+        },
+
+
     },
     created() {
-        console.log('created en MyTable')
         this._fetchAllTasks()
     },
 }
+
+
+
 </script>
 
 <template>
     <div>
-        <h1>Esta es la tabla</h1>
+        <!--         <h1>Lista de tareas</h1>
         <div v-for="todo in tasksList" :key="todo.id">
             <p>{{ todo.title }}</p>
             <span>{{ todo.is_complete ? '✅' : '❌' }}</span>
             <button @click="_handleCompleteTask(todo)">Complete!</button>
             <button @click="_handleEditTask(todo)">Update</button>
-        </div>
+            <button @click="_handleEraseTask(todo)">Erase</button>
+        </div> -->
+
+        <h1>-----------------------</h1>
+<!--         <h1>Completed Tasks</h1>
+        <div v-for="todo in completedTaskList" :key="todo.id">
+            <p>{{ todo.title }}</p>
+            <span>{{ todo.is_complete ? '✅' : '❌' }}</span>
+            <button @click="_handleIncompleteTask(todo)">Incomplete</button>
+            <button @click="_handleEraseTask(todo)">Erase</button>
+        </div> -->
+        <h1>----------------------</h1>
         <div v-if="!updatingTask">
             <input type="text" v-model="newTaskTitle">
-            <button @click="_handleNewTask">Añadir Tarea</button>
+            <button @click="_handleNewTask">Add new task</button>
         </div>
         <div v-else>
             <input type="text" v-model="updateTaskTitle">
             <button @click="_handleUpdateTask">Actualizar Tarea</button>
         </div>
-
-
-
+        <h1>Tasks</h1>
         <table>
             <thead>
                 <tr>
-                    <th></th>
+                    <th>Todos</th>
+                    <th>State</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                <tr v-for="todo in tasksList" :key="todo.id">
+                    <td>{{ todo.title }}</td>
+                    <td>{{ todo.is_complete ? '✅' : '❌' }}</td>
+                    <td>
+                        <button @click="_handleCompleteTask(todo)">Complete!</button>
+                        <button @click="_handleEditTask(todo)">Update</button>
+                        <button @click="_handleEraseTask(todo)">Erase</button>
+                    </td>
                 </tr>
             </tbody>
         </table>
+
+
+
+            <div>
+                <h1>Completed Tasks</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Todos</th>
+                            <th>State</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="todo in completedTaskList" :key="todo.id">
+                            <td>{{ todo.title }}</td>
+                            <td>{{ todo.is_complete ? '✅' : '❌' }}</td>
+                            <td>
+                                <button @click="_handleIncompleteTask(todo)">Incomplete</button>
+                                <button @click="_handleEraseTask(todo)">Erase</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    
+
+
     </div>
 </template>
--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -4,7 +4,8 @@ import supabase from '@/supabase/index.js'
 export default defineStore('tasks', {
   state() {
     return {
-      tasksList: []
+      tasksList: [],
+      completedTaskList: [],
     }
   },
   actions: {
@@ -42,7 +43,57 @@ export default defineStore('tasks', {
         }
       })
     },
+    async _completeTask(task) {
+      const { data, error} = await supabase
+        .from('tasks')
+        .update({ is_complete: true })
+        .eq('id', task.id)
+        .select()
 
+        if (error) throw error;
+
+        const [updatedTask] = data;
+        task.is_complete = updatedTask.is_complete;
+        this.tasksList = this.tasksList.filter(todo => todo.id !== task.id);
+
+    },
+    async _incompleteTask({ id }) {
+      const { data, error } = await supabase
+        .from('tasks')
+        .update({ is_complete: false })
+        .eq('id', id)
+        .select()
+    
+      if (error) throw error;
+    
+      const [task] = data;
+      const index = this.completedTaskList.findIndex(todo => todo.id === task.id)
+      if (index !== -1) {
+        this.completedTaskList.splice(index, 1)
+        this.tasksList.push(task)
+      }
+    },
+    async _eraseTask(id) {
+        const { error } = await supabase
+          .from('tasks')
+          .delete()
+          .eq('id', id)
+        
+        if (error) throw error;
+    },
+    async _fetchTasks() {
+      try {
+          const { data: tasks, error } = await supabase
+              .from('tasks')
+              .select()
+              .order('inserted_at', { ascending: false });
+          if (error) throw error;
+          this.tasksList = tasks.filter(todo => !todo.is_complete);
+          this.completedTaskList = tasks.filter(todo => todo.is_complete);
+      } catch (err) {
+          console.error(err);
+      }
+    }
 
   },
 })
